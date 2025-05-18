069b849711727e739b7474332f40259b30dc9357
bheema-tech <bheemesha590@gmail.com>
2025-05-19 00:25:33 +0530

    hello converstgation

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Trump
 PERSON2 =Musk
 
-echo "$PERSON1: Hey $PERSON2, how are you"
-echo "$PERSON2: Hey $PERSON1, i am good how are you"
+echo "$PERSON1: Hey $PERSON2  how are you"
+echo "$PERSON2: Hey $PERSON1  i am good how are you"
 echo "$PERSON1: I am fine whats up"
 echo "$PERSON2: Hey going to Mars now, Will you come"
