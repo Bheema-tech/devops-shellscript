35e1576fa35cacd3d5fab90349030d60fb39a4d9
bheema-tech <bheemesha590@gmail.com>
2025-05-19 00:36:56 +0530

    hello converstgation

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2  how are you"
+echo "$PERSON2: Hey $PERSON1  i am good how are you"
+echo "$PERSON1: I am fine whats up"
+echo "$PERSON2: Hey going to Mars now, Will you come"
