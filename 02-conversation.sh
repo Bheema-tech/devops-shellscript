fcae04b4214f7ebee39a2501628bd4cce1a38674
bheema-tech <bheemesha590@gmail.com>
2025-05-19 00:10:18 +0530

    hello converstgation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey musk, how are you"
+echo "Musk: Hey Trump, i am good how are you"
+echo "Trump: I am fine whats up"
+echo "Musk: Hey going to Mars now, Will you come"
